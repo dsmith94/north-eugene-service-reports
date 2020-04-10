a7f5ffb36a2de563749bf474d3b48daa8fa2c341
Donald Smith <dsmith94@gmail.com>
2020-04-10 11:08:09 -0700

    One big old glob o' files

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,49 @@
+
+
+var CACHE_NAME = 'my-site-cache-v1';
+var FILES_TO_CACHE = [
+  'index.html',
+  'icons-512.png',
+  'service-reports.js'
+];
+
 self.addEventListener('install', (event) => {
     console.log('👷', 'install', event);
-    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+          console.log('[ServiceWorker] Pre-caching offline page');
+          return cache.addAll(FILES_TO_CACHE);
+        })
+    );
   });
   
   self.addEventListener('activate', (event) => {
     console.log('👷', 'activate', event);
-    return self.clients.claim();
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+              console.log('[ServiceWorker] Removing old cache', key);
+              return caches.delete(key);
+            }
+          }));
+        })
+    );
   });
   
   self.addEventListener('fetch', function(event) {
     console.log('👷', 'fetch', event);
-    //if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-    //    return;
-    //  }
-    event.respondWith(fetch(event.request));
+    if (event.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+      }
+      event.respondWith(
+          fetch(event.request)
+              .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                      return cache.match('offline.html');
+                    });
+              })
+      );
   });
